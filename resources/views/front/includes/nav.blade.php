69a4675778502e4f6b9b263c3034d113b7621e31
aimadkha <aimadkha@gmail.com>
2021-08-12 00:13:28 +0100

    solbe login problem

--- resources/views/front/includes/nav.blade.php
+++ resources/views/front/includes/nav.blade.php
@@ -5,6 +5,10 @@
         <li><a href="#about">About</a></li>
         <li><a href="#menu">Menu</a></li>
         <li><a href="#expert">Expert</a></li>
-        <li><a href="{{route('get.user.login')}}"><i class="fa fa-user" aria-hidden="true"></i> Account</a></li>
+        @auth()
+            <li><a href="{{route('user.profile')}}"><i class="fa fa-user" aria-hidden="true"></i> Account</a></li>
+        @else
+            <li><a href="{{route('get.user.login')}}"><i class="fa fa-user" aria-hidden="true"></i> Login</a></li>
+        @endauth
     </ul>
 </header>
